3a87b21dcd7f4924ed959f9921f45bbbd072546e
jilla sanjay <jillasanjay16@gmail.com>
2026-05-19 23:07:05 +0530

    prepared for deployment

--- app.py
+++ app.py
@@ -149,4 +149,4 @@ if __name__ == "__main__":
     port  = int(os.getenv("FLASK_PORT", 5000))
     debug = os.getenv("FLASK_DEBUG", "true").lower() == "true"
     print(f"MediBot running at http://localhost:{port}")
-    app.run(host="0.0.0.0", port=port, debug=debug)
+    app.run(host="0.0.0.0", port=port)
